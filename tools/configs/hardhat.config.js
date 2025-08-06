5e079688deee274f99531991bbc757b8bf97217c
gabrielee5 <fabiettigabriele@gmail.com>
2025-08-06 13:48:27 +0200

    removed deprecated config

--- tools/configs/hardhat.config.js
+++ tools/configs/hardhat.config.js
@@ -1,13 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "https://eth-sepolia.g.alchemy.com/v2/YOUR-API-KEY";
 const POLYGON_RPC_URL = process.env.POLYGON_RPC_URL || "https://polygon-rpc.com/";
 const POLYGON_AMOY_RPC_URL = process.env.POLYGON_AMOY_RPC_URL || "https://rpc-amoy.polygon.technology/";
-const POLYGON_MUMBAI_RPC_URL = process.env.POLYGON_MUMBAI_RPC_URL || "https://rpc-mumbai.maticvigil.com/";
 const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000000";
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "YOUR-API-KEY";
-const POLYGONSCAN_API_KEY = process.env.POLYGONSCAN_API_KEY || "YOUR-API-KEY";
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -25,13 +22,6 @@ module.exports = {
         accountsBalance: "10000000000000000000000"
       }
     },
-    sepolia: {
-      url: SEPOLIA_RPC_URL,
-      accounts: PRIVATE_KEY !== "0x0000000000000000000000000000000000000000000000000000000000000000" ? [PRIVATE_KEY] : [],
-      chainId: 11155111,
-      blockConfirmations: 6,
-      gasPrice: 20000000000
-    },
     polygon: {
       url: POLYGON_RPC_URL,
       accounts: PRIVATE_KEY !== "0x0000000000000000000000000000000000000000000000000000000000000000" ? [PRIVATE_KEY] : [],
@@ -48,14 +38,6 @@ module.exports = {
       gasPrice: 35000000000, // 35 gwei
       gas: 6000000
     },
-    polygonMumbai: {
-      url: POLYGON_MUMBAI_RPC_URL,
-      accounts: PRIVATE_KEY !== "0x0000000000000000000000000000000000000000000000000000000000000000" ? [PRIVATE_KEY] : [],
-      chainId: 80001,
-      blockConfirmations: 5,
-      gasPrice: 35000000000, // 35 gwei
-      gas: 6000000
-    }
   },
   solidity: {
     version: "0.8.20",
